6603d73a6a1a3424b2cd4b724b87c67da5a7b548
Utsab <utsab1adhikari@gmail.com>
2026-01-19 21:34:32 +0545

    Updated

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, unique: true },
+    name: { type: String, required: true},
+    username: { type: String, required: true, unique: true, lowercase: true },
     email: { type: String, required: true, unique: true },
     password: {
       type: String,
